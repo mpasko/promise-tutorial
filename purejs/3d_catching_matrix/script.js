f5c45f54b01c5ff3a5065caeaf4e705004b78493
Marcin <phisist@op,.pl>
2019-10-24 21:05:55 +0200

    Catching matrix almost finished

--- purejs/3d_catching_matrix/script.js
+++ purejs/3d_catching_matrix/script.js
@@ -14,19 +14,34 @@ function generatePassedRow(id) {
   return `<td id=${id}>${generatePassedIcon()}</td>`;
 }
 
+const sources = ['fulfilled', 'rejected'];
+const middle = ['', '-after-then', '-after-catch', '-after-finally'];
+const verify = ['-then', '-catch', '-finally'];
+
 function initialize() {
   function appendAllFailed(prefix) {
     const element = $('#'+prefix);
-    element.append(generateFailedRow(`${prefix}-then`))
-    element.append(generateFailedRow(`${prefix}-catch`))
-    element.append(generateFailedRow(`${prefix}-finally`));
+    if (!element || element.length < 1) {
+      console.error(`Error! Cannot find ${prefix}!`);
+    }
+    verify.forEach(function (testMethod) {
+      element.append(generateFailedRow(`${prefix}${testMethod}`));
+    });
   }
-  appendAllFailed('fulfilled');
-  appendAllFailed('rejected');
+  sources.forEach(function (source) {
+    middle.forEach(function (intermediate) {
+      appendAllFailed(`${source}${intermediate}`);
+    });
+  });
 }
 
 function assignAsPassed(id) {
-  $(id).empty().append(generatePassedIcon());
+  const element = $(id);
+  if (!element || element.length < 1) {
+    console.error(`Error! Cannot find ${id}!`);
+  }
+  element.empty();
+  element.append(generatePassedIcon());
 }
 
 initialize();
@@ -39,26 +54,125 @@ function generateRejectedPromise() {
   return Promise.reject();
 }
 
-//--------------- it starts here: ---------------
+function switchSource(source) {
+  switch(source) {
+    case 'fulfilled':
+      return generateFulfilledPromise();
+    case 'rejected':
+      return generateRejectedPromise();
+    default:
+      throw new Error(`Unknown source: ${source}`);
+  }
+}
 
+function switchMedium(medium) {
+  switch(medium) {
+    case '':
+      return source => source;
+    case '-after-then':
+      return source => source.then(() => {});
+    case '-after-catch':
+      return source => source.catch(() => {});
+    case '-after-finally':
+      return source => source.finally(() => {});
+    default:
+      throw new Error(`Unknown medium: ${medium}`);
+  }
+}
+
+function switchResult(result, elementId) {
+  switch(result) {
+    case '-then':
+      return source => source.then(() => {
+        assignAsPassed(elementId);
+      });
+    case '-catch':
+      return source => source.catch(() => {
+        assignAsPassed(elementId);
+      });
+    case '-finally':
+      return source => source.finally(() => {
+        assignAsPassed(elementId);
+      });
+    default:
+      throw new Error(`Unknown result: ${result}`);
+  }
+}
+
+function performSpecificExperiment(sourceName, mediumName, resultName) {
+  const elementId = `#${sourceName}${mediumName}${resultName}`;
+  const source = switchSource(sourceName);
+  const medium = switchMedium(mediumName);
+  const result = switchResult(resultName, elementId);
+  result(medium(source));
+}
+
+function performAllExperiments() {
+  sources.forEach(function (source) {
+    middle.forEach(function (intermediate) {
+      verify.forEach(function (testMethod) {
+        performSpecificExperiment(source, intermediate, testMethod);
+      });
+    });
+  });
+}
+
+performAllExperiments();
+
+//--------------- it should generate something like this: ---------------
+/*
 generateFulfilledPromise()
   .then(function () {
     assignAsPassed('#fulfilled-then');
-  })
+  });
+
+generateFulfilledPromise()
   .catch(function () {
     assignAsPassed('#fulfilled-catch');
-  })
+  });
+
+generateFulfilledPromise()
   .finally(function () {
     assignAsPassed('#fulfilled-finally');
   });
 
+//--------------
+
 generateRejectedPromise()
   .then(function () {
     assignAsPassed('#rejected-then');
-  })
+  });
+
+generateRejectedPromise()
   .catch(function () {
     assignAsPassed('#rejected-catch');
-  })
+  });
+
+generateRejectedPromise()
   .finally(function () {
     assignAsPassed('#rejected-finally');
   });
+
+//=========================
+
+generateFulfilledPromise()
+  .then(() => {})
+  .then(function () {
+    assignAsPassed('#fulfilled-after-then-then');
+  });
+
+generateFulfilledPromise()
+  .then(() => {})
+  .catch(function () {
+    assignAsPassed('#fulfilled-after-then-catch');
+  });
+
+generateFulfilledPromise()
+  .then(() => {})
+  .finally(function () {
+    assignAsPassed('#fulfilled-after-then-finally');
+  });
+
+// etc...
+
+  */
